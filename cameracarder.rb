bc9659cb835f75e613ed121fe337da136ca9b754
Dave Rice <dave@dericed.com>
2025-03-14 09:43:04 -0400

    cameracarder 20250315 and add mediainfo

--- cameracarder.rb
+++ cameracarder.rb
@@ -1,12 +1,13 @@
 class Cameracarder < Formula
   desc "report on treesize reports to find potential camera cards"
   homepage "https://github.com/dericed/cameracarder"
-  url "https://github.com/dericed/cameracarder/archive/refs/tags/cameracarder_20250303.tar.gz"
-  sha256 "b3493bba6a709a828d6cdcbf8da3b777200a438a1e099ce9bb32c8ad85a57fdd"
+  url "https://github.com/dericed/cameracarder/archive/refs/tags/cameracarder_20250314.tar.gz"
+  sha256 "00f37fbd1f360bb4a4bc59f86743bfa2ed7714fc2ab9810b961781fdeb05303e"
   head "https://github.com/dericed/cameracarder.git"
 
   depends_on "csvkit"
   depends_on "ffmpeg"
+  depends_on "mediainfo"
   depends_on "xsv"
 
   def install
