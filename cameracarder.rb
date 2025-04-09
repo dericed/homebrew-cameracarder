5d21702c5729094f1117b4dfa8407422b68137cd
Dave Rice <dave@dericed.com>
2025-04-09 18:21:06 -0400

    cameracarder 20250409

--- cameracarder.rb
+++ cameracarder.rb
@@ -1,8 +1,8 @@
 class Cameracarder < Formula
   desc "report on treesize reports to find potential camera cards"
   homepage "https://github.com/dericed/cameracarder"
-  url "https://github.com/dericed/cameracarder/archive/refs/tags/cameracarder_20250314.tar.gz"
-  sha256 "00f37fbd1f360bb4a4bc59f86743bfa2ed7714fc2ab9810b961781fdeb05303e"
+  url "https://github.com/dericed/cameracarder/archive/refs/tags/cameracarder_20250409.tar.gz"
+  sha256 "420abb925c04c49e23982f283c8dcaf8137894279b5108c9e77609fd0248efee"
   head "https://github.com/dericed/cameracarder.git"
 
   depends_on "csvkit"
@@ -14,5 +14,6 @@ class Cameracarder < Formula
     bin.install "camera_cards"
     bin.install "find_cards"
     bin.install "makeproofsheet"
+    bin.install "makexdcam"
   end
 end
