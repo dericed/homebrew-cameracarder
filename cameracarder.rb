bd401001b69a9e5274e9708533a6bbb1c70b7aec
Dave Rice <dave@dericed.com>
2025-04-15 11:24:53 -0400

    cameracarder_20250415 revision

--- cameracarder.rb
+++ cameracarder.rb
@@ -1,9 +1,10 @@
 class Cameracarder < Formula
   desc "report on treesize reports to find potential camera cards"
   homepage "https://github.com/dericed/cameracarder"
-  url "https://github.com/dericed/cameracarder/archive/refs/tags/cameracarder_20250415.tar.gz "
+  url "https://github.com/dericed/cameracarder/archive/refs/tags/cameracarder_20250415.tar.gz"
   sha256 "7c06b6da0761c53fd5df5df07bbdfa5642562b1b8182b7dca5703830e8752de4"
   head "https://github.com/dericed/cameracarder.git"
+  revision 1
 
   depends_on "csvkit"
   depends_on "ffmpeg"
